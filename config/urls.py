937d2675c13abc025c92fb69c47794c1dbf16c7a
yongsuklee <yongsuklee@yongsukleeui-MacBook-Pro.local>
2019-07-05 09:07:35 +0900

    remodify urls.py for bookmark path

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('bookmark.urls')),
+    path('', include('bookmark.urls')),
 
 ]
